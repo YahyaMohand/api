3274cbb5799437d1f3ea0b1c26ef6206858a48f0
YahyaMohand <96312354+YahyaMohand@users.noreply.github.com>
2022-01-26 16:55:05 +0300

    Update credentials for new droplet

--- dbconnect.js
+++ dbconnect.js
@@ -1,9 +1,9 @@
 
 module.exports = {
     'connection': {
-        host: 'ls-6e4cd381121a3bcc4f22bd8f8a610ae850b7eeaf.cvwdz2jidykz.eu-central-1.rds.amazonaws.com',
-        user: 'dbmasteruser',
-        password: '667.-;()4ufaAf=Os^*n*fv#V8VNZv3d',
+        host: 'localhost:8080',
+        user: 'root',
+        password: 'yahya1997',
         database: 'dbmaster',
         multipleStatements: false
     }
