6b7fbf3dfc0a5416bffdfd3e2097d98cc0ff49aa
YahyaMohand <96312354+YahyaMohand@users.noreply.github.com>
2022-02-01 13:01:27 +0300

    amazonDB

--- dbconnect.js
+++ dbconnect.js
@@ -1,9 +1,9 @@
 
 module.exports = {
     'connection': {
-        host: 'localhost:8080',
-        user: 'root',
-        password: 'yahya1997',
+        host: 'ls-6e4cd381121a3bcc4f22bd8f8a610ae850b7eeaf.cvwdz2jidykz.eu-central-1.rds.amazonaws.com',
+        user: 'dbmasteruser',
+        password: '_EEB&n1s..,_5|z$`KgL0`*dUBBV!Z[p',
         database: 'dbmaster',
         multipleStatements: false
     }
